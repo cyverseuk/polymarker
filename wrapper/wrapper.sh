21e6812c434dede50f8d5de9855ba6c3159721e4
Alice Minotto <i.m.e@hotmail.it>
2016-10-26 10:30:04 +0100

    Update wrapper.sh
    fixed for agave case insensitivity

--- wrapper/wrapper.sh
+++ wrapper/wrapper.sh
@@ -37,6 +37,7 @@ if [ -z "${locpFile}" ]
   else
     PRIFILE=${locpFile}
 fi
+
 if [ -z "${CFILE}" ]
   then
     DOCK_IM="cyverseuk/polymarker_wheat:v0.7.3"
@@ -48,14 +49,14 @@ fi
 
 ARGS+=" ${mFile} ${sFile} ${tFile} ${ref} ${GENOMES} ${MINID} ${MODEL} ${ARM} ${PRIFILE} --output output"
 echo ARGS are "${ARGS}"
-CFILE=(${cFile})
-MFILE=(${mFile})
-SFILE=(${sFile})
-TFILE=(${tFile})
-REF=(${ref})
-PRIFILE=(${PRIFILE})
-INPUTS=" ${CFILE[@]:1}, ${MFILE[@]:1}, ${SFILE[@]:1}, ${TFILE[@]:1}, ${REF[@]:1}, ${PRIFILE[@]:1}"
-echo inputs are ${INPUTS}
+CFILEU=(${cFile})
+MFILEU=(${mFile})
+SFILEU=(${sFile})
+TFILEU=(${tFile})
+REFU=(${ref})
+PRIFILEU=(${PRIFILE})
+INPUTSU=" ${CFILEU[@]:1}, ${MFILEU[@]:1}, ${SFILEU[@]:1}, ${TFILEU[@]:1}, ${REFU[@]:1}, ${PRIFILEU[@]:1}"
+echo inputs are ${INPUTSU}
 
 chmod +x launch.sh
 
@@ -63,7 +64,7 @@ echo  universe                = docker >> lib/condorSubmitEdit.htc
 echo docker_image            = ${DOCK_IM} >> lib/condorSubmitEdit.htc
 echo executable               = ./launch.sh >> lib/condorSubmitEdit.htc
 echo arguments                          = ${ARGS} >> lib/condorSubmitEdit.htc
-echo transfer_input_files = ${INPUTS}, launch.sh >> lib/condorSubmitEdit.htc
+echo transfer_input_files = ${INPUTSU}, launch.sh >> lib/condorSubmitEdit.htc
 echo transfer_output_files = output >> lib/condorSubmitEdit.htc
 cat /mnt/data/rosysnake/lib/condorSubmit.htc >> lib/condorSubmitEdit.htc
 
@@ -78,4 +79,4 @@ jobid=`echo $jobid | sed -e 's/\.//'`
 #echo going to monitor job $jobid
 condor_tail -f $jobid
 
-exit 0
+exit 0                  
